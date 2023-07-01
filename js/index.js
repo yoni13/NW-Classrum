ea2a7d6d1af857b8795283227e91cdc0f63707df
Yoni Yang <yoni980807@gmail.com>
2023-07-02 00:14:12 +0800

    5626+

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
+
 function onloaded() {
     const InputArea = document.getElementById('inputarea');
-    InputArea.value = '1.';
+    InputArea.textContent = '1.';
     InputArea.addEventListener('keydown', function (e) {
         if (e.code == 'Enter' || e.code == 'NumpadEnter') {
             e.preventDefault();
-            InputArea.value += '\n';
-            var count = (InputArea.value.match(/\n/g) || []).length;
-            InputArea.value += count + 1 + '.';
-            InputArea.scrollTop = InputArea.scrollHeight
+            InputArea.textContent += '\n';
+            var count = (InputArea.textContent.match(/\n/g) || []).length;
+            InputArea.textContent += count + 1 + '.';
+            InputArea.scrollTop = InputArea.scrollHeight;
+            
         }
         
     });

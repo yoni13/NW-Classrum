af955b55ef9125652f748dd345c42e8c16ee5ff8
Yoni Yang <yoni980807@gmail.com>
2023-07-01 23:42:01 +0800

    bugs

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ function onloaded() {
     const InputArea = document.getElementById('inputarea');
     InputArea.value = '1.';
     InputArea.addEventListener('keydown', function (e) {
-        if (e.code == 'Enter') {
+        if (e.code == 'Enter' || e.code == 'NumpadEnter') {
             e.preventDefault();
             InputArea.value += '\n';
             var count = (InputArea.value.match(/\n/g) || []).length;

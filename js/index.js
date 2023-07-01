905f6610b2f5b65e5a7160fefe501be370953bf4
Yoni Yang <yoni980807@gmail.com>
2023-07-01 23:37:00 +0800

    textarea

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function onloaded() {
+    const InputArea = document.getElementById('inputarea');
+    InputArea.value = '1.';
+    InputArea.addEventListener('keydown', function (e) {
+        if (e.code == 'Enter') {
+            e.preventDefault();
+            InputArea.value += '\n';
+            var count = (InputArea.value.match(/\n/g) || []).length;
+            InputArea.value += count + 1 + '.';
+            InputArea.scrollTop = InputArea.scrollHeight
+        }
+        
+    });
+}

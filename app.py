9d0a14b8a481d30b8f28c44fec1b19dcc46eea6a
yoni13 <yoni980807@gmail.com>
2025-01-30 09:24:51 +0000

    Fix prediction probability calculation to ensure correct subject indexing

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ def MakePred(name):
     new_data = [name]
     new_data_vectorized = vectorizer.transform(new_data).toarray()
     predicted_subject = loaded_model.predict(new_data_vectorized)
-    return label_encoder.inverse_transform(predicted_subject)[0],loaded_model.predict_proba(new_data_vectorized)[0]
+    predicted_proba = loaded_model.predict_proba(new_data_vectorized)[0, predicted_subject[0]]
+    return label_encoder.inverse_transform(predicted_subject)[0], predicted_proba
 
 # Today weekday is an int,subject num is an string,timetable is a dict,
 def GetNextClassWeekday(today_weekday,subject_num,timetable):
